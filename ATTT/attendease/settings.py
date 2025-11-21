5787e120f664f5f21df3f652446d597af065b478
Mansi Shelar <shelarmansi97@gmail.com>
2025-11-21 19:57:36 +0530

    settings updated for deployment

--- ATTT/attendease/settings.py
+++ ATTT/attendease/settings.py
@@ -13,12 +13,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key-here'
+SECRET_KEY = os.environ.get("SECRET_KEY", "test_secret_key_for_local")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*", "attendease.onrender.com"]
+
 
 
 # Application definition
